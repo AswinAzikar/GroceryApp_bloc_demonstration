10c5b6bd191438dc3e61b8a4c0637b4318538771
AswinAzikar <azwinaswin8@gmail.com>
2024-06-04 05:48:30 +0530

    first commit

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,58 @@
+class GroceryData {
+  static List<Map<String, dynamic>> products = [
+    {
+      'id': 1,
+      'name': 'Fresh Apples',
+      'description': 'Crisp and juicy apples',
+      'price': 150.0,
+      'imageUrl': 'https://5.imimg.com/data5/AK/RA/MY-68428614/apple.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Organic Milk',
+      'description': 'Fresh, organic milk from local dairy',
+      'price': 60.0,
+      'imageUrl':
+          'https://www.heritagefoods.in/blog/wp-content/uploads/2020/12/shutterstock_539045662.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Whole Wheat Bread',
+      'description': 'Healthy whole wheat bread',
+      'price': 40.0,
+      'imageUrl':
+          'https://media.npr.org/assets/img/2013/01/11/white-bread-22f499a12d0053112a857332b3d0681628c3d56b.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Basmati Rice',
+      'description': 'Premium quality basmati rice',
+      'price': 120.0,
+      'imageUrl':
+          'https://www.indianhealthyrecipes.com/wp-content/uploads/2023/07/basmati-rice-recipe-500x500.jpg',
+    },
+    {
+      'id': 5,
+      'name': 'Extra Virgin Olive Oil',
+      'description': 'Pure extra virgin olive oil',
+      'price': 500.0,
+      'imageUrl': 'https://m.media-amazon.com/images/I/61bhHDDvqzL.jpg',
+    },
+    {
+      'id': 6,
+      'name': 'Organic Spinach',
+      'description': 'Fresh and organic spinach',
+      'price': 80.0,
+      'imageUrl':
+          'https://cdn.britannica.com/30/82530-050-79911DD4/Spinach-leaves-vitamins-source-person.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Brown Eggs',
+      'description': 'Farm-fresh brown eggs',
+      'price': 3.99,
+      'imageUrl':
+          'https://www.mashed.com/img/gallery/are-brown-eggs-really-worth-the-extra-cost/intro-1597233855.jpg',
+    },
+  ];
+}
